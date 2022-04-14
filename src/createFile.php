c410dc86ab5dd369bbece4235094b384577db6ae
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-04-14 19:14:13 +0300

    Added examples & src

---
+++ src/createFile.php
@@ -0,0 +1,25 @@
+<?php
+
+//get thhe data sent over post
+$data = json_decode(file_get_contents("php://input"), true);
+
+// var_dump($data);
+//generate random name for successive files
+//use $$ to get dynamic filenames
+$fileName = $data['datajson'][0];  
+
+//write to a file
+$jsonfile = "../samples/json/jsonfile-$fileName.json";
+
+
+$file = fopen($jsonfile, 'w');
+fwrite($file, json_encode(array_shift($data['datajson'])));
+fclose($file);
+
+//check if the file exists so as to return a response
+if (file_exists($jsonfile)) {
+    echo "Success: Your .json file is ready at <a class=\"text-green-500 hover:underline\" href=\"$jsonfile\" target=\"_blank\">$jsonfile</a>";
+} else {
+   echo "Error: Something happened and we could not create the .json file";
+}
+
---
+++ src/excel.js
@@ -0,0 +1,47 @@
+let selectedFile;
+document.getElementById('inputfile').addEventListener("change", (event) => {
+    selectedFile = event.target.files[0];
+});
+
+document.getElementById('button').addEventListener("click", () => { 
+    if (selectedFile) {
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+        fileReader.onload = (event) => {
+            let data = event.target.result;
+            let workbook = XLSX.read(data, { type: "binary" });
+            //console.log(workbook);
+
+            var name = selectedFile.name; //filename with .xlsx
+            var fullname = name.replace(".xlsx", ""); //filename without .xlsx
+            //console.log(fullname);
+           
+            //TODO: Implement dynamic filename
+            
+            //initialize an empty variable to hold the data
+            var datajson = [];
+            datajson.push(fullname);
+            workbook.SheetNames.forEach(sheet => {
+                let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+                datajson.push(rowObject);
+                document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject, undefined, 4);
+
+            });
+
+            //added sth to create the 'json' file
+            axios.post('http://localhost:3000/src/createFile.php', { datajson },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        "Access-Control-Allow-Headers": "Content-Type"
+                    }
+                })
+                .then(function (response) {
+                    document.getElementById("response").innerHTML = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+});
