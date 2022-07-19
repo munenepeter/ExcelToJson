274bfa3a08923f4c966904cdd3467b7fa59d2b65
munenepeter <peternjega7@gmail.com>
2022-07-19 12:30:45 +0300

    doesn't work

--- src/excel.js
+++ src/excel.js
@@ -3,45 +3,53 @@ document.getElementById('inputfile').addEventListener("change", (event) => {
     selectedFile = event.target.files[0];
 });
 
-document.getElementById('button').addEventListener("click", () => { 
+document.getElementById('button').addEventListener("click", () => {
+    let excelData = parseExcel(selectedFile);
+    console.log(parseExcel(selectedFile));
+    document.getElementById("jsondata").innerHTML = JSON.stringify(excelData, undefined, 4);
+});
+
+function postData(datajson) {
+    axios.post('http://localhost:8989/src/createFile.php', {
+            datajson
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Headers": "Content-Type"
+            }
+        })
+        .then(function (response) {
+            document.getElementById("response").innerHTML = response.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function parseExcel(selectedFile) {
     if (selectedFile) {
+        var datajson = [];
         let fileReader = new FileReader();
         fileReader.readAsBinaryString(selectedFile);
         fileReader.onload = (event) => {
             let data = event.target.result;
-            let workbook = XLSX.read(data, { type: "binary" });
+            let workbook = XLSX.read(data, {
+                type: "binary"
+            });
             //console.log(workbook);
 
             var name = selectedFile.name; //filename with .xlsx
             var fullname = name.replace(".xlsx", ""); //filename without .xlsx
-            //console.log(fullname);
            
-            //TODO: Implement dynamic filename
-            
-            //initialize an empty variable to hold the data
-            var datajson = [];
             datajson.push(fullname);
+            
             workbook.SheetNames.forEach(sheet => {
                 let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
-                datajson.push(rowObject);
-                document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject, undefined, 4);
-
+                datajson.push(rowObject); 
+                 return datajson;
             });
-
-            //added sth to create the 'json' file
-            axios.post('http://localhost:8989/src/createFile.php', { datajson },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "Access-Control-Allow-Headers": "Content-Type"
-                    }
-                })
-                .then(function (response) {
-                    document.getElementById("response").innerHTML = response.data;
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+          
         }
+      
     }
-});
+}
