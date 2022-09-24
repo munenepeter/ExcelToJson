67933b757424373af0553a8d25e1471bd60de0e2
munenepeter <peternjega7@gmail.com>
2022-09-24 13:51:36 +0300

    finally upload works

--- src/createFile.php
+++ src/createFile.php
@@ -1,28 +1,15 @@
 <?php
 
 //get thhe data sent over post
-$data = json_decode(file_get_contents("php://input"), true);
+//$data = json_encode(file_get_contents("php://input"));
 
-//generate random name for successive files
-//use $$ to get dynamic filenames
-$fileName = preg_replace('/\s+/', '-', $data['datajson'][0]);  
-$$fileName = trim($fileName);
+$data = $_FILES;
+var_dump($_FILES['excelFile']["tmp_name"]);
 
-//write to a file
-$jsonfile = "../samples/json/{$$fileName}.json";
-array_shift($data['datajson']);
-array_unshift($data['datajson'], [
-    'File Name' => $$fileName
-]);
-
-$file = fopen($jsonfile, 'w');
-fwrite($file, json_encode($data));
-fclose($file);
-
-//check if the file exists so as to return a response
-if (file_exists($jsonfile)) {
-    echo "Success: Your .json file is ready at <a class=\"text-green-500 hover:underline\" href=\"$jsonfile\" target=\"_blank\">$jsonfile</a>";
-} else {
-   echo "Error: Something happened and we could not create the .json file";
-}
+// //check if the file exists so as to return a response
+// if (file_exists($jsonfile)) {
+//     echo "Success: Your .json file is ready at <a class=\"text-green-500 hover:underline\" href=\"$jsonfile\" target=\"_blank\">$jsonfile</a>";
+// } else {
+//    echo "Error: Something happened and we could not create the .json file";
+// }
 
